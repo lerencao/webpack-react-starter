dff3a51d8590dbf7308c94340278ae87ef526892
Cao Jiafeng <funfriendcjf@gmail.com>
2015-12-05 22:32:18 +0800

    init commit

---
+++ app/index.js
@@ -0,0 +1,3 @@
+import './index.css';
+
+// boot your js code here
---
+++ webpack.dev.config.js
@@ -0,0 +1,51 @@
+var path = require('path');
+var webpack = require('webpack');
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build'),
+  public: path.join(__dirname, 'public')
+};
+
+module.exports = {
+  entry: path.resolve(PATHS.app, 'index.js'),
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js',
+    publicPath: '/javascripts'
+  },
+  
+  devServer: {
+    contentBase: PATHS.public,
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true,
+    
+    host: process.env.HOST,
+    port: process.env.PORT
+  },
+  
+  module: {
+    loaders: [
+     { 
+       test: /\.css$/,
+        loader: 'style-loader!css-loader' 
+      },
+      { 
+        test: /\.(png|jpg|woff|woff2)$/, 
+        loader: 'url-loader?limit=8192'
+      }, // inline base64 URLs for <=8k images, direct URLs for the rest
+      { 
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+        loader: "file-loader" 
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
